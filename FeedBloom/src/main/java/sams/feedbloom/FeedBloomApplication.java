3ff6d15f05c146a4a934a580b4c6c88a45f5c0fc
sams52s <sams52tas@gmail.com>
2025-03-07 01:28:09 +0600

    Project created

---
+++ FeedBloom/src/main/java/sams/feedbloom/FeedBloomApplication.java
@@ -0,0 +1,13 @@
+package sams.feedbloom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedBloomApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FeedBloomApplication.class, args);
+	}
+	
+}
---
+++ FeedBloom/src/test/java/sams/feedbloom/FeedBloomApplicationTests.java
@@ -0,0 +1,13 @@
+package sams.feedbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedBloomApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
