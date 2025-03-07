ee8f369220e5ebdba36c414ea18de8e91c88ba7e
sams52s <sams52tas@gmail.com>
2025-03-07 22:46:10 +0600

    Refactor security filter chain method name

--- FeedBloom/src/main/java/sams/feedbloom/authentication/config/SecurityConfig.java
+++ FeedBloom/src/main/java/sams/feedbloom/authentication/config/SecurityConfig.java
@@ -3,13 +3,11 @@ package sams.feedbloom.authentication.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 	
 	@Bean
@@ -18,7 +16,7 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	public SecurityFilterChain springSecurityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 				.authorizeHttpRequests(auth -> auth
 						                               .requestMatchers("/register/**").permitAll()
