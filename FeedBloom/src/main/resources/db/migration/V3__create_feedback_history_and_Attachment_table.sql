b0343428dd2459f6b02ed60578adc5d7c07028ac
sams52s <sams52tas@gmail.com>
2025-03-13 10:21:33 +0600

    update

--- FeedBloom/src/main/resources/db/migration/V3__create_feedback_history_and_Attachment_table.sql
+++ FeedBloom/src/main/resources/db/migration/V3__create_feedback_history_and_Attachment_table.sql
@@ -30,21 +30,3 @@ CREATE TABLE feedback_attachment
 );
 
 CREATE INDEX idx_feedback_attachment_feedback_id ON feedback_attachment (feedback_id);
-
-CREATE TABLE approval
-(
-    id              BIGSERIAL PRIMARY KEY,
-    feedback_id     BIGINT  NOT NULL,
-    approved_by     BIGINT  NOT NULL,
-    approval_status BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at      TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMP        DEFAULT NULL,
-    is_deleted      BOOLEAN          DEFAULT FALSE,
-    deleted_at      TIMESTAMP        DEFAULT NULL,
-    deleted_by      VARCHAR(100)     DEFAULT NULL,
-    FOREIGN KEY (feedback_id) REFERENCES feedback (id) ON DELETE CASCADE,
-    FOREIGN KEY (approved_by) REFERENCES users (id) ON DELETE CASCADE
-);
-
-CREATE INDEX idx_approval_feedback_id ON approval (feedback_id);
-CREATE INDEX idx_approval_approved_by ON approval (approved_by);
