37489948bd48ec178c6c3b48fc5f272e23d9ae91
sams52s <sams52tas@gmail.com>
2025-03-17 00:26:27 +0600

    Refactor FeedbackController to remove unused AuthInfoGenerator and streamline user info retrieval

--- FeedBloom/src/main/java/sams/feedbloom/feedback/controller/FeedbackController.java
+++ FeedBloom/src/main/java/sams/feedbloom/feedback/controller/FeedbackController.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import sams.feedbloom.authentication.dto.UserDTO;
-import sams.feedbloom.authentication.service.AuthInfoGenerator;
 import sams.feedbloom.authentication.service.AuthService;
 import sams.feedbloom.feedback.dto.FeedbackResponse;
 import sams.feedbloom.feedback.entity.FeedbackCategory;
@@ -19,17 +18,13 @@ import java.util.List;
 @RequestMapping("/feedbacks")
 @RequiredArgsConstructor
 public class FeedbackController {
-	private final AuthInfoGenerator authInfoGenerator;
 	private final AuthService authService;
 	private final FeedbackService feedbackService;
 	
 	@GetMapping("/dashboard")
 	public String showDashboard(Model model) {
-		UserDTO user = authService.getAuthenticatedUserInfo();
-		authInfoGenerator.loadAuthenticatedUserInfo(user);
-		boolean approved = !authInfoGenerator.getAccountNotFound() && authInfoGenerator.getApproved();
-		model.addAttribute("user", user);
-		model.addAttribute("UserApproved", approved);
+		UserDTO userInfo = authService.getAuthenticatedUserInfo();
+		model.addAttribute("user", userInfo);
 		model.addAttribute("category", FeedbackCategory.values());
 		model.addAttribute("priority", FeedbackPriority.values());
 		model.addAttribute("status", FeedbackStatus.values());
