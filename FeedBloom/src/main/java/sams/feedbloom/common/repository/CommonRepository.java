c92aec7eaed6b87e76ff8227c56ec38162668027
sams52s <sams52tas@gmail.com>
2025-03-18 05:01:33 +0600

    Refactor CommonRepository to extend CommonEntity for consistency with entity structure

--- FeedBloom/src/main/java/sams/feedbloom/common/repository/CommonRepository.java
+++ FeedBloom/src/main/java/sams/feedbloom/common/repository/CommonRepository.java
@@ -2,11 +2,11 @@ package sams.feedbloom.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import sams.feedbloom.common.entity.Common;
+import sams.feedbloom.common.entity.CommonEntity;
 
 import java.util.List;
 
 @NoRepositoryBean
-public interface CommonRepository<T extends Common, ID> extends JpaRepository<T, ID> {
+public interface CommonRepository<T extends CommonEntity, ID> extends JpaRepository<T, ID> {
 	List<T> findByIsDeletedFalse();
 }
