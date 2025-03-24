7a9554620d2792ca5477b2d87c504acb687e600d
sams52s <sams52tas@gmail.com>
2025-03-24 16:59:17 +0600

    removed unused files

--- FeedBloom/src/main/java/sams/feedbloom/feedback/repository/FeedbackAttachmentRepository.java
+++
@@ -1,7 +0,0 @@
-package sams.feedbloom.feedback.repository;
-
-import sams.feedbloom.common.repository.CommonRepository;
-import sams.feedbloom.feedback.entity.FeedbackAttachment;
-
-public interface FeedbackAttachmentRepository extends CommonRepository<FeedbackAttachment, Long> {
-}
