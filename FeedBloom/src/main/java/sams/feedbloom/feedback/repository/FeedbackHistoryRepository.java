85ea956522ff124ac9b5f568f033a19b7b949afd
sams52s <sams52tas@gmail.com>
2025-03-25 12:53:58 +0600

    Refactor ApprovalService and Feedback-related classes; remove unused methods and update return types to LinkedList

--- FeedBloom/src/main/java/sams/feedbloom/feedback/repository/FeedbackHistoryRepository.java
+++ FeedBloom/src/main/java/sams/feedbloom/feedback/repository/FeedbackHistoryRepository.java
@@ -3,5 +3,8 @@ package sams.feedbloom.feedback.repository;
 import sams.feedbloom.common.repository.CommonRepository;
 import sams.feedbloom.feedback.entity.FeedbackHistory;
 
+import java.util.LinkedList;
+
 public interface FeedbackHistoryRepository extends CommonRepository<FeedbackHistory, Long> {
+	LinkedList<FeedbackHistory> findByIsDeletedFalseAndFeedbackIdOrderByCreatedAtAsc(Long feedbackId);
 }
--- FeedBloom/src/main/java/sams/feedbloom/feedback/repository/FeedbackRepository.java
+++ FeedBloom/src/main/java/sams/feedbloom/feedback/repository/FeedbackRepository.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Repository;
 import sams.feedbloom.common.repository.CommonRepository;
 import sams.feedbloom.feedback.entity.Feedback;
 
-import java.util.List;
+import java.util.LinkedList;
 
 @Repository
 public interface FeedbackRepository extends CommonRepository<Feedback, Long> {
-	List<Feedback> findByIsDeletedAndCreatedByOrderByCreatedAtAsc(Boolean isDeleted, String createdBy);
+	LinkedList<Feedback> findByIsDeletedAndCreatedByOrderByCreatedAtAsc(Boolean isDeleted, String createdBy);
 	
 	Feedback findByIsDeletedFalseAndId(Long id);
 }
